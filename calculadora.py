147a79d878128f414c142cc5e0ab02702145e171
Matheus De Oliveira Nascimento Santos <matheusdeons2022@gmail.com>
2025-06-10 17:19:39 -0300

    Add files via upload

---
+++ calculadora.py
@@ -0,0 +1,12 @@
+a = int(input("Digite um número"))
+b = int(input("Digite um número"))
+
+soma = a + b
+subtracao = a - b
+multiplicacao = a + b
+divisao = a / b
+
+print("A soma será: " , soma)
+print("A soma será: " , subtracao )
+print("A soma será: " , multiplicacao)
+print("A soma será: " , divisao)
